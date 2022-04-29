6f2c51af521247d6ed30a4a400a9799c51051763
Shambhavi-Pardhi <shambhavipardhi@gmail.com>
2022-04-29 23:10:16 +0530

    backend3

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-
-import { AppComponent } from './app.component';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-=======
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule, Routes } from '@angular/router';
@@ -44,7 +31,6 @@ import { LoggingService } from './logging.service';
     ApiService,
     LoggingService
   ],
->>>>>>> 4c489f7f12e7a0d07d5a73a4d212a972c1eb9049
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- question.js
+++ question.js
@@ -4,7 +4,7 @@ const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "DB-Project",
-    password: "",
+    password: "sunflower050115",
     port: 5432,
 })
 
